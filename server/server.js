58cf82c26526f1a26c53ac37f01bf9bdd55c2824
Nihar Mane <95009543+Nihar16@users.noreply.github.com>
2025-11-17 18:28:23 -0800

    Updating file

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const { GraphQLServer } = require('graphql-yoga');
 
 const typeDefs = `
+  type Message {
+    id: ID!
+    content: String! 
+  }
+
   type Query {
-    hello(name: String): String!
+    messages: [Message!]!
   }
 `;
 
+const Resolvers = {
+    Query: {
+        messages: () => MESSAGES,
+    }
+};
 
+const Port = 4000;
 
 const server = new GraphQLServer();
 server.start(() => console.log(`Server is running on http://localhost:${Port}/`));
